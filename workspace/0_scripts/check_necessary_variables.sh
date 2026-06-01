1f6c07f5822d1c94a7d9e46cbee06b7bf69a992f
Mario PC <somsos112@gmail.com>
2026-06-01 17:23:58 -0600

    added .m2 to volume for jenkins aswell

--- workspace/0_scripts/check_necessary_variables.sh
+++ workspace/0_scripts/check_necessary_variables.sh
@@ -15,6 +15,7 @@ function check_necessary_variables {
     
     if [ "$1" = "JENKINS"  ]; then
         echo "In pipeline"
+        HOME=/home/jenkins
     
     elif [ "$1" = "CONTAINER-SHELL"  ]; then
         echo "In container"
