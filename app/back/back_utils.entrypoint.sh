b781063503f945c0d0496c4ce3f3e7ca9a03e30f
Mario PC <somsos112@gmail.com>
2026-06-03 12:50:07 -0600

    added badges for success and failing tests

--- app/back/back_utils.entrypoint.sh
+++ app/back/back_utils.entrypoint.sh
@@ -1,4 +1,4 @@
-set -e
+#set -e
 #set -x # show executed lines
 
 # REFERENCE
@@ -10,14 +10,23 @@ if [[ "$1" != "unitary_tests" && "$1" != "integration_tests" && "$1" != "e2e_tes
     echo "[ERROR]* set first parameter to either \"unitary_tests\", \"integration_tests\", \"e2e_tests\""
     exit 1
 fi
+TEST_TYPE="$1"
+#LOG_FILE="/opt/template51/tests_overall/unitary_tests.log"
+#mvn test &>> $LOG_FILE
 
-if [[ "$1" == "unitary_tests" ]]; then
-    #LOG_FILE="/opt/template51/tests_overall/unitary_tests.log"
-    echo -e "\033[38;5;27;48;5;231m [MAIN] Start running unitary tests. \033[0m"
-    #mvn test &>> $LOG_FILE
+# TEST_RESULT
+echo "[MAIN] Start running unitary tests."
+
+if [[ "$TEST_TYPE" == "unitary_tests" ]]; then
+    echo "[MAIN] Running unitary tests."
     mvn test -Dspring.profiles.active=test
-    echo -e "\033[38;5;27;48;5;231m [MAIN] END running unitary tests. \033[0m"
+    TEST_RESULT=$?
 fi
+
+echo "[MAIN] Finished running unitary tests with result: $TEST_RESULT."
+
+
+exit $TEST_RESULT
 
 
 echo "http://jenkins.mariomv-local.org/userContent/backend-tests-result.svg"
